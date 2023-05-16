be66b04b8bf2cd6dc59a1100581e561e6a16d6b0
hyoin08 <benjaminkim0228@gmail.com>
2023-05-16 20:11:17 +0900

    changes 2023/05/26

--- src/routers/rootRouter.js
+++ src/routers/rootRouter.js
@@ -1,12 +1,17 @@
 import express from "express";
-import { getJoin, postJoin, login } from "../controllers/userController";
+import {
+  getJoin,
+  postJoin,
+  getlogin,
+  postLogin,
+} from "../controllers/userController";
 import { home, search } from "../controllers/videoController";
 
 const rootRouter = express.Router();
 
 rootRouter.get("/", home);
 rootRouter.route("/join").get(getJoin).post(postJoin);
-rootRouter.get("/login", login);
+rootRouter.route("/login").get(getlogin).post(postLogin);
 rootRouter.get("/search", search);
 
 export default rootRouter;
