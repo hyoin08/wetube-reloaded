1018b2fe3595c149ebd048ed6af93c9004f5e5eb
hyoin08 <benjaminkim0228@gmail.com>
2023-06-05 20:28:57 +0900

    changes 2023/06/05

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -132,11 +132,39 @@ export const finishGithubLogin = async (req, res) => {
     return res.redirect("/login");
   }
 };
-
-export const edit = (req, res) => res.send("Edit User");
-export const remove = (req, res) => res.send("Remove User");
+export const getEdit = (req, res) => {
+  return res.render("edit-profile", { pageTitle: "Edit Profile" });
+};
+export const postEdit = async (req, res) => {
+  const {
+    session: {
+      user: { _id },
+    },
+    body: { name, email, username, location },
+  } = req;
+  const updatedUser = await User.findByIdAndUpdate(
+    _id,
+    {
+      name,
+      email,
+      username,
+      location,
+    },
+    { new: true }
+  );
+  req.session.user = updatedUser;
+  return res.redirect("/users/edit");
+};
 export const logout = (req, res) => {
   req.session.destroy();
   return res.redirect("/");
 };
+
+export const getChangePassword = (req, res, next) => {
+  return res.render("change-password");
+};
+export const postChangePassword = (req, res, next) => {
+  return res.redirect("/");
+};
+
 export const see = (req, res) => res.send("See User");
